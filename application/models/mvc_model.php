488b86a1c9a4288835a4452452d406356c9dedfd
AndiAlifs <andyalyfsyah4@gmail.com>
2022-11-22 17:18:01 +0700

    make mvc model

---
+++ application/models/mvc_model.php
@@ -0,0 +1,21 @@
+<!-- make mvc model -->
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class mvc_model extends CI_Model {
+
+    public function __construct () {
+        parent::__construct();
+    }
+
+    public function getAggregasi()
+    {   
+    $query = "SELECT category, count(*)
+            FROM data, kategori 
+            WHERE data.kategori_id = kategori.id
+            GROUP BY category"; 
+    $result = $this->db->query($query);
+    return $result->result_array();
+}
+
+}
